f1e2bb30e6a082230204dc74720546c99dae68df
WafaBergaoui <wafabergaoui3@gmail.com>
2022-02-01 15:54:53 +0100

    backend added

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+import http from 'http';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import userRouter from './routers/userRouter.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(()=>{
+    console.log("conected to mongodb");
+  }).catch(error => {
+    console.log("mongo error",error);
+  });
+
+app.use('/api/users', userRouter);
+
+app.get('/api/config/google', (req, res) => {
+  res.send(process.env.GOOGLE_API_KEY || '');
+});
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+
+const httpServer = http.Server(app);
+const users = [];
+
+
+httpServer.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
+
+// app.listen(port, () => {
+//   console.log(`Serve at http://localhost:${port}`);
+// });
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { register } from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
+export default function Register(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "Right",
+        alignItems: "Right",
+        height: "100vh",
+      }}
+    >
+      <form className="form">
+        <div>
+          <h1>Create Account</h1>
+        </div>
+
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter name"
+            required
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Enter confirm password"
+            required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Register
+          </button>
+        </div>
+        <div>
+          <label />
+          <div>
+            Already have an account? <Link to={`/signin`}>Sign-In</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
